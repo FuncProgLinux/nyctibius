ef9b387c11cdbd9892c119036b4e9979b1de16d5
FuncProgLinux <softwarelibre@urutau-ltd.org>
2025-11-30 00:15:34 -0600

    Import more sources

--- README.md
+++ README.md
@@ -16,7 +16,10 @@ GitHub.
 > Starting from Dec 1. This channel will merge with the Guix MATE channel, and
 > package development will continue from here. This is to focus on a single
 > channel and be fast with updates on other tools.
-
+>
+> If you still wish to rely on the MATE software available, install this channel
+> and replace all your `use-modules` or `#:use-module` from `(guix-mate packages
+> something) to (nyctibius packages something)
 
 ## Usage
 
@@ -24,10 +27,28 @@ Organization members can refer the original forge's Wiki for the usage snippet.
 
 The same goes for bug reports and package requests.
 
+> I don't care! I still want to use it!
+
+Fine, but don't say I didn't warn you...
+
+1. Clone the repository from whatever mirror you are using. I'll assume your
+   checkout will live at `$HOME/src/nyctibius/`
+2. Add the subscription snippet to your `/etc/channels.scm` or
+   `~/.config/guix/channels.scm`:
+
+```scheme
+(channel
+    (name 'nyctibius-local)
+    (branch "stable")
+    (url (string-append "file://"
+                        (getenv "HOME") "/src/nyctibius")))
+```
+
 ## Software here
 
 The channel contains the following software
 
+- Latest MATE Desktop packages
 - Missing GNU Emacs packages
 - Latest FreeFem builds
 - Some InfoSec tools
--- channel/nyctibius/packages/mint.scm
+++ channel/nyctibius/packages/mint.scm
@@ -1,3 +1,28 @@
+;; mint.scm --- Linux Mint packages for Nyctibius -*- mode: scheme; -*-
+;;
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; Copyright © 2025 Urutau-Ltd <softwarelibre@urutau-ltd.org>
+;;
+;;   , _ ,      _    _            _                     _ _      _
+;;  ( o o )    | |  | |          | |                   | | |    | |
+;; /'` ' `'\   | |  | |_ __ _   _| |_ __ _ _   _ ______| | |_ __| |
+;; |'''''''|   | |  | | '__| | | | __/ _` | | | |______| | __/ _` |
+;; |\\'''//|   | |__| | |  | |_| | || (_| | |_| |      | | || (_| |
+;;    """       \____/|_|   \__,_|\__\__,_|\__,_|      |_|\__\__,_|
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 (define-module (nyctibius packages mint)
   #:use-module (guix)
   #:use-module (guix gexp)
