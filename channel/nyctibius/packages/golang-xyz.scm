fc0cb2f262ef06e0b122c0b204ee9be4e74f458e
FuncProgLinux <softwarelibre@urutau-ltd.org>
2025-12-04 13:12:15 -0600

    Nyctibius: add go-github-com-awnumar-memguard

--- channel/nyctibius/packages/golang-xyz.scm
+++ channel/nyctibius/packages/golang-xyz.scm
@@ -32,6 +32,7 @@
     #:use-module (guix build-system go)
     #:use-module (gnu packages)
     #:use-module (gnu packages golang)
+    #:use-module (gnu packages golang-build)
     #:use-module (gnu packages golang-check)
     #:use-module (gnu packages golang-xyz))
 
@@ -59,3 +60,82 @@
     (description
      "Package jsonschema uses reflection to generate JSON Schemas from Go types")
     (license license:expat)))
+
+;; MemGuard Libraries
+(define-public go-github-com-awnumar-memcall
+  (package
+    (name "go-github-com-awnumar-memcall")
+    (version "0.5.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/awnumar/memcall")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "13iri047l53xlcgxgzp165hf2n2wfx1lm73803ipn3z3yhwmndw2"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/awnumar/memcall"))
+    (propagated-inputs (list go-golang-org-x-sys))
+    (home-page "https://github.com/awnumar/memcall")
+    (synopsis "memcall")
+    (description
+     "This package provides a cross-platform wrapper over some common memory-related
+system calls.")
+    (license license:asl2.0)))
+
+(define-public go-lukechampine-com-frand
+  (package
+    (name "go-lukechampine-com-frand")
+    (version "1.5.1")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/lukechampine/frand")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "10dspd6xh7gic8ih7frqvfqqzbnsk9grvqd5djyic173i1jphcpl"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "lukechampine.com/frand"))
+    (home-page "https://lukechampine.com/frand")
+    (synopsis "frand")
+    (description
+     "@@code{frand} is a fast CSPRNG in userspace, implemented as a lightweight
+wrapper around the @@url{https://go.dev/src/math/rand/v2/chacha8.go,(code
+(math/rand/v2).@code{ChaCha8})} generator.  The initial cipher key is derived
+from the kernel CSPRNG, after which .")
+    (license license:expat)))
+
+(define-public go-github-com-awnumar-memguard
+  (package
+    (name "go-github-com-awnumar-memguard")
+    (version "0.23.0")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/awnumar/memguard")
+             (commit (string-append "v" version))))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1s20frd5k2d5lz9jyiw8x0xwkz7sg4b30q4ckds5lqwafcylcbw2"))))
+    (build-system go-build-system)
+    (arguments
+     (list
+      #:import-path "github.com/awnumar/memguard"))
+    (propagated-inputs (list go-lukechampine-com-frand go-golang-org-x-sys
+                             go-golang-org-x-crypto
+                             go-github-com-awnumar-memcall))
+    (home-page "https://github.com/awnumar/memguard")
+    (synopsis "MemGuard")
+    (description
+     "Package memguard implements a secure software enclave for the storage of
+sensitive information in memory.")
+    (license license:asl2.0)))
