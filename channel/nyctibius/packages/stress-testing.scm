d31883f5b8d6efe2a75735e0c0440fd02d0371cc
FuncProgLinux <softwarelibre@urutau-ltd.org>
2025-12-05 20:23:51 -0600

    Nyctibius: Fix import path for go dep

--- channel/nyctibius/packages/stress-testing.scm
+++ channel/nyctibius/packages/stress-testing.scm
@@ -33,6 +33,7 @@
     #:use-module (gnu packages)
     #:use-module (gnu packages golang)
     #:use-module (gnu packages golang-build)
+    #:use-module (gnu packages golang-web)
     #:use-module (gnu packages golang-xyz)
     #:use-module (nyctibius packages golang-xyz))
 
